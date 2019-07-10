6f5c76ec3097f0d675f809bb8c00e984785e18b3
Erik Sandstedt <goosefraba19@gmail.com>
2019-07-10 21:21:16 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,15 @@ import { ModelImpl, Model } from "./model";
 import { compile } from "./compile";
 
 export { Model } from "./model";
+export { Coordinate } from "./model/coordinate";
+export { Country } from "./model/country";
+export { Faction } from "./model/faction";
+export { Leader } from "./model/leader";
+export { Planet } from "./model/planet";
+export { Player } from "./model/player";
+export { Pop } from "./model/pop";
+export { Species } from "./model/species";
+export { System, Hyperlane } from "./model/system";
 
 export function from(data: string | ArrayBuffer | Blob): Promise<Model> {
   return compile(data).then(doc => new ModelImpl(doc));
