c3a9a40e96436f63f3435c214b8afc8c2a25b997
Erik Sandstedt <erik@sandstedt.eu>
2019-09-28 00:01:10 +0200

    resources in the budget

--- src/model/country/budget.ts
+++ src/model/country/budget.ts
@@ -9,12 +9,31 @@ export class BudgetImpl {
     var balance = asPairArray(currentMonth["balance"])
       .map(x =>
         asPairArray(x.value).map(y => ({
-          type: x.key,
-          resource: y.key,
+          type: x.key as string,
+          resource: y.key as string,
           amount: parseFloat(asString(y.value))
         }))
       )
       .reduce((a, b) => a.concat(b));
+
+    var resources: any = {};
+
+    balance.forEach(({ type, resource, amount }) => {
+      if (!resources[resource]) {
+        resources[resource] = {
+          items: [],
+          total: 0
+        };
+      }
+
+      resources[resource].items.push({
+        type,
+        amount
+      });
+      resources[resource].total += amount;
+    });
+
+    console.log(JSON.stringify(resources));
 
     //balance.forEach(x => console.log(x.resource, x.amount, x.type));
   }
